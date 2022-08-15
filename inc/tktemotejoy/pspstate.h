a9c8e3c58507de7771c7128c9f4426e4e096b40f
tekuto <g.tekuto@gmail.com>
2022-08-16 02:25:03 +0900

    PspState#diff()のテストを修正

--- inc/tktemotejoy/pspstate.h
+++ inc/tktemotejoy/pspstate.h
@@ -6,8 +6,26 @@
 class PspState
 {
 public:
+    enum Button
+    {
+        UP = 0x0010,
+        DOWN = 0x0040,
+        LEFT = 0x0080,
+        RIGHT = 0x0020,
+
+        CIRCLE = 0x2000,
+        CROSS = 0x4000,
+        TRIANGLE = 0x1000,
+        SQUARE = 0x8000,
+        TRIGGER_L = 0x0100,
+        TRIGGER_R = 0x0200,
+        START = 0x0008,
+        SELECT = 0x0001,
+    };
+
     using Bits = unsigned int;
 
+    using Buttons = unsigned short;
     using Axis = unsigned char;
 
     using WhenDiff = std::function<
@@ -29,40 +47,8 @@ public:
         , const WhenDiff &
     ) const;
 
-    void pressUp(
-    );
-
-    void pressDown(
-    );
-
-    void pressLeft(
-    );
-
-    void pressRight(
-    );
-
-    void pressCircle(
-    );
-
-    void pressCross(
-    );
-
-    void pressTriangle(
-    );
-
-    void pressSquare(
-    );
-
-    void pressTriggerL(
-    );
-
-    void pressTriggerR(
-    );
-
-    void pressStart(
-    );
-
-    void pressSelect(
+    void pressButtons(
+        const Buttons
     );
 
     void operateAxisX(
--- src/tktemotejoy/pspstate.cpp
+++ src/tktemotejoy/pspstate.cpp
@@ -3,33 +3,15 @@
 namespace {
     struct BitField
     {
-        bool    select : 1;
-
-        bool : 2;
-
-        bool    start : 1;
-        bool    up : 1;
-        bool    right : 1;
-        bool    down : 1;
-        bool    left : 1;
-        bool    triggerL : 1;
-        bool    triggerR : 1;
-
-        bool : 2;
-
-        bool    triangle : 1;
-        bool    circle : 1;
-        bool    cross : 1;
-        bool    square : 1;
-
-        unsigned char axisX;
-        unsigned char axisY;
+        alignas( 1 ) unsigned short buttons;
+        alignas( 1 ) unsigned char  axisX;
+        alignas( 1 ) unsigned char  axisY;
     };
 }
 
 #define SET_BITS( _NAME, _VALUE ) reinterpret_cast< BitField & >( this->bits )._NAME = _VALUE;
 
-#define PRESS_BUTTON( _NAME ) SET_BITS( _NAME, 1 )
+#define PRESS_BUTTON( _NAME ) SET_BITS( _NAME, true )
 
 #define OPERATE_AXIS( _NAME, _VALUE ) SET_BITS( _NAME, _VALUE )
 
@@ -43,76 +25,11 @@ void PspState::diff(
     }
 }
 
-void PspState::pressUp(
+void PspState::pressButtons(
+    const PspState::Buttons _BUTTONS
 )
 {
-    PRESS_BUTTON( up )
-}
-
-void PspState::pressDown(
-)
-{
-    PRESS_BUTTON( down )
-}
-
-void PspState::pressLeft(
-)
-{
-    PRESS_BUTTON( left )
-}
-
-void PspState::pressRight(
-)
-{
-    PRESS_BUTTON( right )
-}
-
-void PspState::pressCircle(
-)
-{
-    PRESS_BUTTON( circle )
-}
-
-void PspState::pressCross(
-)
-{
-    PRESS_BUTTON( cross )
-}
-
-void PspState::pressTriangle(
-)
-{
-    PRESS_BUTTON( triangle )
-}
-
-void PspState::pressSquare(
-)
-{
-    PRESS_BUTTON( square )
-}
-
-void PspState::pressTriggerL(
-)
-{
-    PRESS_BUTTON( triggerL )
-}
-
-void PspState::pressTriggerR(
-)
-{
-    PRESS_BUTTON( triggerR )
-}
-
-void PspState::pressStart(
-)
-{
-    PRESS_BUTTON( start )
-}
-
-void PspState::pressSelect(
-)
-{
-    PRESS_BUTTON( select )
+    //TODO
 }
 
 void PspState::operateAxisX(
