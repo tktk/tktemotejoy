11dff4a6716142e1d38600cd14f9a6a99325bf04
tekuto <g.tekuto@gmail.com>
2022-08-17 15:11:38 +0900

    jsEventAxisToPspStateAxis()の処理を実装

--- src/tktemotejoy/common.cpp
+++ src/tktemotejoy/common.cpp
@@ -2,10 +2,16 @@
 #include "tktemotejoy/pspstate.h"
 #include <linux/joystick.h>
 
+namespace {
+    enum {
+        JS_EVENT_AXIS_ZERO = 0x8000,
+        SHIFT_TO_PSP_STATE_AXIS = 8,
+    };
+}
+
 PspState::Axis jsEventAxisToPspStateAxis(
     const __s16 _JS_EVENT_AXIS
 )
 {
-    //TODO
-    return 0xff;
+    return ( _JS_EVENT_AXIS + JS_EVENT_AXIS_ZERO ) >> SHIFT_TO_PSP_STATE_AXIS;
 }
