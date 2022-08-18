410d4fb40a40bc231380cab38bc914390cb0d962
tekuto <g.tekuto@gmail.com>
2022-08-18 20:47:13 +0900

    Mappings#joystickStateToPspState()の処理を実装

--- src/tktemotejoy/mappings.cpp
+++ src/tktemotejoy/mappings.cpp
@@ -1,13 +1,15 @@
 #include "tktemotejoy/mappings.h"
 #include "tktemotejoy/pspstate.h"
 #include "tktemotejoy/joystickstate.h"
+#include <utility>
 
 Mappings::Mappings(
     Mappings::Impl &&                   _impl
     , const Mappings::Impl::size_type   _DEFAULT_MAPPING_INDEX
 )
+    : IMPL( std::move( _impl ) )
+    , mappingIndex( _DEFAULT_MAPPING_INDEX )
 {
-    //TODO
 }
 
 void Mappings::joystickStateToPspState(
@@ -15,5 +17,40 @@ void Mappings::joystickStateToPspState(
     , const JoystickState & _JOYSTICK_STATE
 )
 {
-    //TODO
+    _JOYSTICK_STATE.forPressedButtons(
+        [
+            this
+            , &_pspState
+        ]
+        (
+            const JoystickState::States::size_type  _INDEX
+        ) -> bool
+        {
+            this->IMPL.at( 0 ).pressButton(
+                _INDEX
+                , _pspState
+            );
+
+            return false;
+        }
+    );
+    _JOYSTICK_STATE.forAxes(
+        [
+            this
+            , &_pspState
+        ]
+        (
+            const JoystickState::States::size_type      _INDEX
+            , const JoystickState::States::value_type   _VALUE
+        ) -> bool
+        {
+            this->IMPL.at( 0 ).operateAxis(
+                _INDEX
+                , _VALUE
+                , _pspState
+            );
+
+            return false;
+        }
+    );
 }
