7d8a03ab83f9a026c32d7761d245f693b38045e3
tekuto <g.tekuto@gmail.com>
2022-08-20 02:27:51 +0900

    generateToButtonsUnique()のテストを追加

--- src/tktemotejoy/generatehandler/tobuttonstest.cpp
+++ src/tktemotejoy/generatehandler/tobuttonstest.cpp
@@ -47,16 +47,156 @@ TEST_F(
     );
 }
 
-//TODO Left
-//TODO Right
-//TODO Circle
-//TODO Cross
-//TODO Triangle
-//TODO Square
-//TODO TriggerL
-//TODO TriggerR
-//TODO Start
-//TODO Select
+TEST_F(
+    GenerateToButtonsTest
+    , Left
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "left"
+    ]
+})"
+        , 0x80800080
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , Right
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "right"
+    ]
+})"
+        , 0x80800020
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , Circle
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "circle"
+    ]
+})"
+        , 0x80802000
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , Cross
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "cross"
+    ]
+})"
+        , 0x80804000
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , Triangle
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "triangle"
+    ]
+})"
+        , 0x80801000
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , Square
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "square"
+    ]
+})"
+        , 0x80808000
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , TriggerL
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "triggerL"
+    ]
+})"
+        , 0x80800100
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , TriggerR
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "triggerR"
+    ]
+})"
+        , 0x80800200
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , Start
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "start"
+    ]
+})"
+        , 0x80800008
+    );
+}
+
+TEST_F(
+    GenerateToButtonsTest
+    , Select
+)
+{
+    this->test(
+        R"({
+    "buttons" : [
+        "select"
+    ]
+})"
+        , 0x80800001
+    );
+}
+
 //TODO Buttons
 //TODO IllegalButton
 //TODO NotExistsButtons
