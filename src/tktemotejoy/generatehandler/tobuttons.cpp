a09e2759f7c17d3f5e898b40909744ec91dc5403
tekuto <g.tekuto@gmail.com>
2022-08-20 02:21:50 +0900

    generateToButtonsUnique()の処理を修正

--- src/tktemotejoy/generatehandler/tobuttons.cpp
+++ src/tktemotejoy/generatehandler/tobuttons.cpp
@@ -1,12 +1,29 @@
 #include "tktemotejoy/generatehandler/tobuttons.h"
 #include "tktemotejoy/handler/forpspstate/tobuttons.h"
 #include "tktemotejoy/mapping.h"
+#include "tktemotejoy/pspstate.h"
 #include "tktemotejoy/customjson.h"
+
+namespace {
+    const auto  KEY_BUTTONS = "buttons";
+}
 
 Mapping::PressButtonHandlerForPspStateUnique generateToButtonsUnique(
     const Json::object_t &  _OBJECT
 )
 {
-    //TODO
-    return Mapping::handlerUnique( new ToButtons( 0x0010 ) );
+    const auto &    STRINGS_JSON = _OBJECT.at( KEY_BUTTONS );
+
+    const auto &    STRINGS = STRINGS_JSON.get_ref< const Json::array_t & >();
+
+    const auto  STRING_TO_BUTTON = std::map< std::string, PspState::Buttons >(
+        {
+            { "up", PspState::Button::UP },
+            { "down", PspState::Button::DOWN },
+        }
+    );
+
+    const auto  IT = STRING_TO_BUTTON.find( STRINGS.at( 0 ) );
+
+    return Mapping::handlerUnique( new ToButtons( IT->second ) );
 }
