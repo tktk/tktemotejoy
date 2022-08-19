da223bfeb1bac029cf72c4854c0c1b3ab390d13e
tekuto <g.tekuto@gmail.com>
2022-08-19 23:59:58 +0900

    parseCustomJson()の処理を修正

--- src/tktemotejoy/customjson.cpp
+++ src/tktemotejoy/customjson.cpp
@@ -6,20 +6,20 @@ Json parseCustomJson(
     const std::string & _STRING
 )
 {
-    const auto  REMOVE_BLOCK_COMMENT = std::regex_replace(
-        _STRING
-        , std::regex( R"(/\*(?:.|\s)*\*/)" )
-        , ""
-    );
-
     const auto  REMOVE_LINE_COMMENT = std::regex_replace(
-        REMOVE_BLOCK_COMMENT
+        _STRING
         , std::regex( R"(\s*//.*)" )
         , ""
     );
 
-    const auto  REMOVE_TAIL_COMMA = std::regex_replace(
+    const auto  REMOVE_BLOCK_COMMENT = std::regex_replace(
         REMOVE_LINE_COMMENT
+        , std::regex( R"(/\*(?:.|\s)*\*/)" )
+        , ""
+    );
+
+    const auto  REMOVE_TAIL_COMMA = std::regex_replace(
+        REMOVE_BLOCK_COMMENT
         , std::regex( R"(,(\s*[\]\}]))" )
         , "$1"
     );
