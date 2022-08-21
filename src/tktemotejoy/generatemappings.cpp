e062d1820b38cbc26bb8303b37e02470f13cf72e
tekuto <g.tekuto@gmail.com>
2022-08-21 16:28:04 +0900

    generateMappings()の仮実装を追加

--- src/tktemotejoy/generatemappings.cpp
+++ src/tktemotejoy/generatemappings.cpp
@@ -1,14 +1,18 @@
 #include "tktemotejoy/generatemappings.h"
 #include "tktemotejoy/mappings.h"
 #include "tktemotejoy/customjson.h"
+#include <utility>
 
 Mappings generateMappings(
     const Json &    _JSON
 )
 {
     //TODO
+    auto    impl = Mappings::Impl();
+    impl.push_back( Mapping() );
+
     return Mappings(
-        Mappings::Impl()
+        std::move( impl )
         , 0
     );
 }
