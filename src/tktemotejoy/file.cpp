f1b8b6a6a9fa536464e9763d4075b28a31016a96
tekuto <g.tekuto@gmail.com>
2022-08-22 19:49:36 +0900

    readFile()の処理を実装

--- src/tktemotejoy/file.cpp
+++ src/tktemotejoy/file.cpp
@@ -1,10 +1,25 @@
 #include "tktemotejoy/file.h"
 #include <string>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+#include <stdexcept>
 
 std::string readFile(
     const std::string & _FILE_NAME
 )
 {
-    //TODO
-    return std::string( "TESTFILE" );
+    auto    stream = std::ifstream( _FILE_NAME );
+    if( stream.fail() == true ) {
+        auto    oStringStream = std::ostringstream();
+
+        oStringStream << "ファイル" << '"' << _FILE_NAME << '"' << "が存在しない";
+
+        throw std::runtime_error( oStringStream.str() );
+    }
+
+    return std::string(
+        std::istreambuf_iterator< char >( stream )
+        , std::istreambuf_iterator< char >()
+    );
 }
