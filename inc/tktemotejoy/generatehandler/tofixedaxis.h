327fc972feba6906e722825c0015aafa947f936d
tekuto <g.tekuto@gmail.com>
2022-08-24 02:01:02 +0900

    GenerateToFixedAxisUnique#operator()()の処理を実装

--- inc/tktemotejoy/generatehandler/tofixedaxis.h
+++ inc/tktemotejoy/generatehandler/tofixedaxis.h
@@ -2,6 +2,7 @@
 #define TKTEMOTEJOY_GENERATEHANDLER_TOFIXEDAXIS_H
 
 #include "tktemotejoy/customjson.h"
+#include "tktemotejoy/jsonerror.h"
 #include <linux/joystick.h>
 #include <string>
 
@@ -13,28 +14,28 @@ public:
         const Json::object_t &  _OBJECT
     ) const
     {
-        //TODO
-/*
         const auto  KEY_VALUE = std::string( "value" );
 
         __s16   value;
 
         const auto  IT = _OBJECT.find( KEY_VALUE );
         if( IT == _OBJECT.end() ) {
-            //TODO 例外投げる
+            throw jsonIsNotExists( KEY_VALUE );
         } else {
             const auto &    VALUE_JSON = IT->second;
+            //TODO
+/*
             if( VALUE_JSON.is_number_integer() == false ) {
                 throw jsonIsNotInteger( KEY_VALUE );
             }
+*/
 
             const auto &    VALUE = VALUE_JSON.get_ref< const Json::number_integer_t & >();
 
             value = VALUE;
         }
-*/
 
-        return GENERATE_TO_FIXED_AXIS_UNIQUE_T()( 10 );
+        return GENERATE_TO_FIXED_AXIS_UNIQUE_T()( value );
     }
 };
 
