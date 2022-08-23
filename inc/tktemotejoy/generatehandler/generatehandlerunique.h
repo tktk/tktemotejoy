f3e5c5203d50090f07fcb551c720848eb79a04b7
tekuto <g.tekuto@gmail.com>
2022-08-23 15:38:42 +0900

    GenerateHandlerWithDeadZoneUnique#operator()()の処理を実装

--- inc/tktemotejoy/generatehandler/generatehandlerunique.h
+++ inc/tktemotejoy/generatehandler/generatehandlerunique.h
@@ -2,6 +2,7 @@
 #define TKTEMOTEJOY_GENERATEHANDLER_GENERATEHANDLERUNIQUE_H
 
 #include "tktemotejoy/customjson.h"
+#include <string>
 
 template<
     typename HANDLER_UNIQUE_T
@@ -12,14 +13,16 @@ HANDLER_UNIQUE_T generateHandlerUnique(
     const Json::object_t &  _OBJECT
 )
 {
-    const auto  IT = _OBJECT.find( "type" );
+    const auto  KEY_TYPE = std::string( "type" );
+
+    const auto  IT = _OBJECT.find( KEY_TYPE );
     if( IT == _OBJECT.end() ) {
         return HANDLER_UNIQUE_T();
     }
 
     const auto &    TYPE = IT->second;
     if( TYPE.is_string() != true ) {
-        return HANDLER_UNIQUE_T();
+        return HANDLER_UNIQUE_T();  //TODO 例外を投げる
     } else if( TYPE.get_ref< const Json::string_t & >() != GET_TYPE_T()() ) {
         return HANDLER_UNIQUE_T();
     }
--- inc/tktemotejoy/generatehandler/withdeadzone.h
+++ inc/tktemotejoy/generatehandler/withdeadzone.h
@@ -2,6 +2,7 @@
 #define TKTEMOTEJOY_GENERATEHANDLER_WITHDEADZONE_H
 
 #include "tktemotejoy/customjson.h"
+#include <string>
 
 template< typename T >
 class GenerateHandlerWithDeadZoneUnique
@@ -11,10 +12,22 @@ public:
         const Json::object_t &  _OBJECT
     ) const
     {
-        //TODO
+        const auto  KEY_DEAD_ZONE = std::string( "deadZone" );
+
+        __s16   deadZone;
+
+        const auto  IT = _OBJECT.find( KEY_DEAD_ZONE );
+        if( IT == _OBJECT.end() ) {
+            deadZone = 0;
+        } else {
+            const auto &    DEAD_ZONE = IT->second.get_ref< const Json::number_integer_t & >();
+
+            deadZone = DEAD_ZONE;
+        }
+
         return static_cast< const T * >( this )->operatorCallImpl(
             _OBJECT
-            , 10
+            , deadZone
         );
     }
 };
