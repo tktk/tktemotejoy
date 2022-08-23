e373cbfb73fd27193ae799ee13c8f09e04c5f455
tekuto <g.tekuto@gmail.com>
2022-08-24 00:09:37 +0900

    DummyOperateAxisHandlerForPspStateを追加

--- src/tktemotejoy/handler/forpspstate/dummytest.cpp
+++ src/tktemotejoy/handler/forpspstate/dummytest.cpp
@@ -1,5 +1,7 @@
 #include "tktemotejoy/test.h"
 #include "tktemotejoy/handler/forpspstate/dummy.h"
+#include "tktemotejoy/pspstate.h"
+#include <linux/joystick.h>
 
 TEST(
     DummyPressButtonHandlerForPspStateTest
@@ -11,6 +13,40 @@ TEST(
     auto    pspState = PspState();
 
     dummy( pspState );
+
+    const auto  OTHER = PspState();
+
+    auto    calledWhenDiff = false;
+
+    pspState.diff(
+        OTHER
+        , [
+            &calledWhenDiff
+        ]
+        (
+            const PspState::Bits &
+        )
+        {
+            calledWhenDiff = true;
+        }
+    );
+
+    EXPECT_FALSE( calledWhenDiff );
+}
+
+TEST(
+    DummyOperateAxisHandlerForPspStateTest
+    , OperatorCall
+)
+{
+    auto    dummy = DummyOperateAxisHandlerForPspState();
+
+    auto    pspState = PspState();
+
+    dummy(
+        10
+        , pspState
+    );
 
     const auto  OTHER = PspState();
 
