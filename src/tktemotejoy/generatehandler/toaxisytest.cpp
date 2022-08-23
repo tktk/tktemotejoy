1a579d3f06312521545d0aecc4493a20f014a4ff
tekuto <g.tekuto@gmail.com>
2022-08-23 23:19:55 +0900

    generateToAxisYUnique()のテストを修正

--- src/tktemotejoy/generatehandler/toaxisytest.cpp
+++ src/tktemotejoy/generatehandler/toaxisytest.cpp
@@ -1,42 +1,22 @@
 #include "tktemotejoy/test.h"
 #include "tktemotejoy/generatehandler/toaxisy.h"
+#include "tktemotejoy/generatehandler/operateaxishandlerforpspstatetest.h"
 #include "tktemotejoy/customjson.h"
-#include <linux/joystick.h>
-#include <string>
 
 namespace {
-    struct TestToAxisY
+    struct GenerateToAxisYUnique
     {
-        const void * DUMMY;
-        const __s16 DEAD_ZONE;
-        const __s16 MAX;
-    };
-
-    class GenerateToAxisYTest : public ::testing::Test
-    {
-    public:
-        void test(
-            const std::string & _JSON_STRING
-            , const __s16       _EXPECTED_DEAD_ZONE
-            , const __s16       _EXPECTED_MAX
-        )
+        auto operator()(
+            const Json::object_t &  _OBJECT
+        ) const
         {
-            const auto  JSON = Json::parse( _JSON_STRING );
-
-            const auto &    OBJECT = JSON.get_ref< const Json::object_t & >();
-
-            auto    handlerUnique = generateToAxisYUnique( OBJECT );
-            ASSERT_NE( nullptr, handlerUnique.get() );
-
-            const auto &    TEST_TO_AXIS_Y = reinterpret_cast< const TestToAxisY & >( *handlerUnique );
-
-            EXPECT_EQ( _EXPECTED_DEAD_ZONE, TEST_TO_AXIS_Y.DEAD_ZONE );
-            EXPECT_EQ( _EXPECTED_MAX, TEST_TO_AXIS_Y.MAX );
+            return generateToAxisYUnique( _OBJECT );
         }
     };
+
+    using GenerateToAxisYTest = GenerateOperateAxisHandlerForPspStateTestTmpl< GenerateToAxisYUnique >;
 }
 
-//TODO ToAxisYが生成できているかテストできていない
 TEST_F(
     GenerateToAxisYTest
     , Standard
@@ -48,7 +28,7 @@ TEST_F(
     "deadZone" : 10,
     "max" : 20
 })"
-        , 10
         , 20
+        , 0xff800000
     );
 }
