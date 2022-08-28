2bda3915b0342988eb638aa22b7780ed49c1e1cb
tekuto <g.tekuto@gmail.com>
2022-08-28 18:09:07 +0900

    GenerateChangeMappingUnique#operator()()の処理を修正

--- inc/tktemotejoy/generatehandler/changemapping.h
+++ inc/tktemotejoy/generatehandler/changemapping.h
@@ -2,9 +2,8 @@
 #define TKTEMOTEJOY_GENERATEHANDLER_CHANGEMAPPING_H
 
 #include "tktemotejoy/customjson.h"
-#include "tktemotejoy/jsonerror.h"
+#include "tktemotejoy/json.h"
 #include <string>
-#include <cstddef>
 
 template< typename GENERATE_CHANGE_MAPPING_UNIQUE_T >
 class GenerateChangeMappingUnique
@@ -16,17 +15,10 @@ public:
     {
         const auto  KEY_MAPPING = std::string( "mapping" );
 
-        const auto  IT = _OBJECT.find( KEY_MAPPING );
-        if( IT == _OBJECT.end() ) {
-            throw jsonIsNotExists( KEY_MAPPING );
-        }
-
-        const auto &    MAPPING_JSON = IT->second;
-        if( MAPPING_JSON.is_number_unsigned() == false ) {
-            throw jsonIsNotUnsigned( KEY_MAPPING );
-        }
-
-        const auto &    MAPPING = MAPPING_JSON.get_ref< const Json::number_integer_t & >();
+        const auto &    MAPPING = getJsonUnsignedFromObject(
+            _OBJECT
+            , KEY_MAPPING
+        );
 
         return GENERATE_CHANGE_MAPPING_UNIQUE_T()( MAPPING );
     }
