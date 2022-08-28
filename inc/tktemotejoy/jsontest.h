0e4b32761bd2a74e57567f912a4f8de641b9ee41
tekuto <g.tekuto@gmail.com>
2022-08-28 21:43:16 +0900

    getJsonIntegerFromObject()のテストを修正

---
+++ inc/tktemotejoy/jsontest.h
@@ -0,0 +1,60 @@
+#ifndef TKTEMOTEJOY_JSONTEST_H
+#define TKTEMOTEJOY_JSONTEST_H
+
+#include "tktemotejoy/test.h"
+#include "tktemotejoy/customjson.h"
+#include <string>
+#include <stdexcept>
+
+template< typename GET_JSON_T >
+class GetJsonTest : public ::testing::Test
+{
+public:
+    template< typename EXPECTED_T >
+    void test(
+        const std::string &     _JSON_STRING
+        , const std::string &   _KEY
+        , const EXPECTED_T &    _EXPECTED
+    ) const
+    {
+        const auto  JSON = Json::parse( _JSON_STRING );
+
+        const auto &    OBJECT = JSON.get_ref< const Json::object_t & >();
+
+        EXPECT_EQ(
+            _EXPECTED
+            , GET_JSON_T()(
+                OBJECT
+                , _KEY
+            )
+        );
+    }
+
+    void testAnyThrow(
+        const std::string &     _JSON_STRING
+        , const std::string &   _KEY
+        , const std::string &   _PARENT_KEY1
+        , const std::string &   _PARENT_KEY2
+        , const std::string &   _EXPECTED_WHAT
+    )
+    {
+        const auto  JSON = Json::parse( _JSON_STRING );
+
+        const auto &    OBJECT = JSON.get_ref< const Json::object_t & >();
+
+        try {
+            GET_JSON_T()(
+                OBJECT
+                , _KEY
+                , _PARENT_KEY1
+                , _PARENT_KEY2
+            );
+
+            ASSERT_FALSE( true );   // ここに到達してはいけない
+        } catch( const std::runtime_error & _EX ) {
+            EXPECT_STREQ( _EXPECTED_WHAT.c_str(), _EX.what() );
+        }
+    }
+};
+
+#endif  // TKTEMOTEJOY_JSONTEST_H
--- src/tktemotejoy/json_getjsonintegertest.cpp
+++ src/tktemotejoy/json_getjsonintegertest.cpp
@@ -1,58 +1,28 @@
 #include "tktemotejoy/test.h"
 #include "tktemotejoy/json.h"
+#include "tktemotejoy/jsontest.h"
 #include "tktemotejoy/customjson.h"
 #include <string>
-#include <stdexcept>
 
 namespace {
-    class GetJsonIntegerTest : public ::testing::Test
+    struct GetJsonInteger
     {
-    public:
-        void test(
-            const std::string &                 _JSON_STRING
-            , const std::string &               _KEY
-            , const Json::number_integer_t &    _EXPECTED_INTEGER
+        template< typename ... PARENT_KEYS_T >
+        const auto & operator()(
+            const Json::object_t &      _OBJECT
+            , const std::string &       _KEY
+            , const PARENT_KEYS_T & ... _PARENT_KEYS
         ) const
         {
-            const auto  JSON = Json::parse( _JSON_STRING );
-
-            const auto &    OBJECT = JSON.get_ref< const Json::object_t & >();
-
-            EXPECT_EQ(
-                _EXPECTED_INTEGER
-                , getJsonIntegerFromObject(
-                    OBJECT
-                    , _KEY
-                )
+            return getJsonIntegerFromObject(
+                _OBJECT
+                , _KEY
+                , _PARENT_KEYS ...
             );
         }
-
-        void testAnyThrow(
-            const std::string &     _JSON_STRING
-            , const std::string &   _KEY
-            , const std::string &   _PARENT_KEY1
-            , const std::string &   _PARENT_KEY2
-            , const std::string &   _EXPECTED_WHAT
-        )
-        {
-            const auto  JSON = Json::parse( _JSON_STRING );
-
-            const auto &    OBJECT = JSON.get_ref< const Json::object_t & >();
-
-            try {
-                getJsonIntegerFromObject(
-                    OBJECT
-                    , _KEY
-                    , _PARENT_KEY1
-                    , _PARENT_KEY2
-                );
-
-                ASSERT_FALSE( true );   // ここに到達してはいけない
-            } catch( const std::runtime_error & _EX ) {
-                EXPECT_STREQ( _EXPECTED_WHAT.c_str(), _EX.what() );
-            }
-        }
     };
+
+    using GetJsonIntegerTest = GetJsonTest< GetJsonInteger >;
 }
 
 TEST_F(
