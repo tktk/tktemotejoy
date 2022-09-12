d18f7a3942b80a3f0c769ee4e3635c05b7297eb7
tekuto <g.tekuto@gmail.com>
2022-09-12 13:39:54 +0900

    main()の処理を修正

--- src/tktemotejoy/main.cpp
+++ src/tktemotejoy/main.cpp
@@ -12,6 +12,23 @@
 #include <linux/input.h>
 
 namespace {
+    template< typename INDICES_T >
+    std::size_t countAvailableIndices(
+        const INDICES_T &   _INDICES
+    )
+    {
+        return std::count_if(
+            _INDICES.begin()
+            , _INDICES.end()
+            , [](
+                const int & _INDEX
+            ) -> bool
+            {
+                return _INDEX >= 0;
+            }
+        );
+    }
+
     Mappings generateMappingsFromFile(
         const std::string &     _FILE_PATH
         , const std::size_t &   _BUTTONS
@@ -30,16 +47,23 @@ namespace {
     }
 
     void initializeJoystickState(
-        const int           _DESCRIPTOR
-        , JoystickState &   _joystickState
+        const int                   _DESCRIPTOR
+        , const EvdevKeyIndices &   _KEY_INDICES
+        , const EvdevAbsIndices &   _ABS_INDICES
+        , JoystickState &           _joystickState
     )
     {
         const auto  KEY_STATES = generateEvdevKeyStates( _DESCRIPTOR );
 
         const auto  KEY_STATES_SIZE = KEY_STATES.size();
         for( auto i = std::size_t( 0 ) ; i < KEY_STATES_SIZE ; i++ ) {
+            const auto &    INDEX = _KEY_INDICES.at( i );
+            if( INDEX < 0 ) {
+                continue;
+            }
+
             _joystickState.setButtonState(
-                i
+                INDEX
                 , KEY_STATES.test( i ) == true ? 1 : 0
             );
         }
@@ -48,8 +72,13 @@ namespace {
 
         const auto  ABS_DATA_ARRAY_SIZE = ABS_DATA_ARRAY.size();
         for( auto i = std::size_t( 0 ) ; i < ABS_DATA_ARRAY_SIZE ; i++ ) {
+            const auto &    INDEX = _ABS_INDICES.at( i );
+            if( INDEX < 0 ) {
+                continue;
+            }
+
             _joystickState.setAxisState(
-                i
+                INDEX
                 , ABS_DATA_ARRAY.at( i ).value
             );
         }
@@ -70,8 +99,17 @@ int main(
         return 1;
     }
 
-    const auto  BUTTONS = EVDEV_BUTTONS;
-    const auto  AXES = EVDEV_AXES;
+    int evdev;
+    const auto  EVDEV_CLOSER = openEvdev(
+        evdev
+        , options.deviceFilePath
+    );
+
+    const auto  KEY_INDICES = generateEvdevKeyIndices( evdev );
+    const auto  ABS_INDICES = generateEvdevAbsIndices( evdev );
+
+    const auto  BUTTONS = countAvailableIndices( KEY_INDICES );
+    const auto  AXES = countAvailableIndices( ABS_INDICES );
 
     auto    mappings = generateMappingsFromFile(
         options.mapFilePath
@@ -86,12 +124,6 @@ int main(
         , options.port
     );
 
-    int evdev;
-    const auto  JOYSTICK_CLOSER = openEvdev(
-        evdev
-        , options.deviceFilePath
-    );
-
     auto    joystickState = JoystickState(
         BUTTONS
         , AXES
@@ -99,6 +131,8 @@ int main(
 
     initializeJoystickState(
         evdev
+        , KEY_INDICES
+        , ABS_INDICES
         , joystickState
     );
 
@@ -118,7 +152,9 @@ int main(
             INPUT_EVENTS_BEGIN
             , INPUT_EVENTS_BEGIN + READ_EVENTS
             , [
-                &joystickState
+                &KEY_INDICES
+                , &ABS_INDICES
+                , &joystickState
             ]
             (
                 const input_event & _EVENT
@@ -129,13 +165,31 @@ int main(
                 const auto &    EVENT_VALUE = _EVENT.value;
 
                 if( EVENT_TYPE == EV_KEY ) {
+                    const auto &    INDEX = KEY_INDICES.at( EVENT_CODE );
+                    if( INDEX < 0 ) {
+                        auto    oStringStream = std::ostringstream();
+
+                        oStringStream << "無効なキーコード : [" << EVENT_CODE << ']';
+
+                        throw std::runtime_error( oStringStream.str() );
+                    }
+
                     joystickState.setButtonState(
-                        EVENT_CODE
+                        INDEX
                         , EVENT_VALUE
                     );
                 } else if( EVENT_TYPE == EV_ABS ) {
+                    const auto &    INDEX = ABS_INDICES.at( EVENT_CODE );
+                    if( INDEX < 0 ) {
+                        auto    oStringStream = std::ostringstream();
+
+                        oStringStream << "無効な軸コード : [" << EVENT_CODE << ']';
+
+                        throw std::runtime_error( oStringStream.str() );
+                    }
+
                     joystickState.setAxisState(
-                        EVENT_CODE
+                        INDEX
                         , EVENT_VALUE
                     );
                 }
