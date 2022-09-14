9ff80ae19f61342246550158ed54cf1a026c6ea1
tekuto <g.tekuto@gmail.com>
2022-09-14 21:01:34 +0900

    GenerateToAxisUnique_new#operator()()のテストを追加

--- inc/tktemotejoy/generatehandler/toaxis.h
+++ inc/tktemotejoy/generatehandler/toaxis.h
@@ -1,12 +1,47 @@
 #ifndef TKTEMOTEJOY_GENERATEHANDLER_TOAXIS_H
 #define TKTEMOTEJOY_GENERATEHANDLER_TOAXIS_H
 
+#include "tktemotejoy/generatehandler/withrange.h"
 #include "tktemotejoy/generatehandler/withdeadzone.h"
 #include "tktemotejoy/customjson.h"
 #include "tktemotejoy/json.h"
-#include <linux/joystick.h>
+#include <linux/input.h>
 #include <string>
 
+template< typename GENERATE_TO_AXIS_UNIQUE_T >
+class GenerateToAxisUnique_new : public GenerateHandlerWithRangeUnique< GenerateToAxisUnique_new< GENERATE_TO_AXIS_UNIQUE_T > >
+{
+public:
+    auto generateHandler(
+        const Json::object_t &  _OBJECT
+        , const __s16           _MIN
+        , const __s16           _MAX
+        , const __s16           _DEAD_ZONE
+    ) const
+    {
+/*
+        const auto  KEY_MAX_ = std::string( "max" );
+        const auto  DEFAULT_MAX = Json::number_integer_t( 0x7fff );
+
+        const auto &    MAX = getJsonIntegerFromObjectWithDefault(
+            _OBJECT
+            , DEFAULT_MAX
+            , KEY_MAX_
+        );
+
+        const auto  MAX_S16 = __s16( MAX );
+*/
+
+        return GENERATE_TO_AXIS_UNIQUE_T()(
+            100
+            , 200
+            , 300
+            , 400
+        );
+    }
+};
+
+//REMOVEME
 template< typename GENERATE_TO_AXIS_UNIQUE_T >
 class GenerateToAxisUnique : public GenerateHandlerWithDeadZoneUnique< GenerateToAxisUnique< GENERATE_TO_AXIS_UNIQUE_T > >
 {
--- src/tktemotejoy/generatehandler/toaxistest.cpp
+++ src/tktemotejoy/generatehandler/toaxistest.cpp
@@ -1,17 +1,85 @@
 #include "tktemotejoy/test.h"
 #include "tktemotejoy/generatehandler/toaxis.h"
 #include "tktemotejoy/customjson.h"
-#include <linux/joystick.h>
+#include <linux/input.h>
 #include <string>
 #include <memory>
 
 namespace {
+    struct TestHandler_new
+    {
+        const __s16 MIN;
+        const __s16 MAX;
+        const __s16 DEAD_ZONE;
+        const __s16 LIMIT;
+    };
+
+    struct TestGenerateToAxisUnique_new
+    {
+        auto operator()(
+            const __s16     _MIN
+            , const __s16   _MAX
+            , const __s16   _DEAD_ZONE
+            , const __s16   _LIMIT
+        ) const
+        {
+            return std::unique_ptr< TestHandler_new >(
+                new TestHandler_new{
+                    _MIN
+                    , _MAX
+                    , _DEAD_ZONE
+                    , _LIMIT
+                }
+            );
+        }
+    };
+
+    using TestGenerateHandlerUnique_new = GenerateToAxisUnique_new< TestGenerateToAxisUnique_new >;
+
+    class GenerateToAxisUnique_newTest : public ::testing::Test
+    {
+    public:
+        void test(
+            const std::string & _JSON_STRING
+            , const __s16       _EXPECTED_MIN
+            , const __s16       _EXPECTED_MAX
+            , const __s16       _EXPECTED_DEAD_ZONE
+            , const __s16       _EXPECTED_LIMIT
+        ) const
+        {
+            const auto  JSON = Json::parse( _JSON_STRING );
+
+            const auto &    OBJECT = JSON.get_ref< const Json::object_t & >();
+
+            auto    handlerUnique = TestGenerateHandlerUnique_new()( OBJECT );
+            ASSERT_NE( nullptr, handlerUnique.get() );
+
+            EXPECT_EQ( _EXPECTED_MIN, handlerUnique->MIN );
+            EXPECT_EQ( _EXPECTED_MAX, handlerUnique->MAX );
+            EXPECT_EQ( _EXPECTED_DEAD_ZONE, handlerUnique->DEAD_ZONE );
+            EXPECT_EQ( _EXPECTED_LIMIT, handlerUnique->LIMIT );
+        }
+
+        void testAnyThrow(
+            const std::string & _JSON_STRING
+        ) const
+        {
+            const auto  JSON = Json::parse( _JSON_STRING );
+
+            const auto &    OBJECT = JSON.get_ref< const Json::object_t & >();
+
+            EXPECT_ANY_THROW( TestGenerateHandlerUnique_new()( OBJECT ) );
+        }
+    };
+
+    //REMOVEME
     struct TestHandler
     {
         const __s16 DEAD_ZONE;
         const __s16 MAX;
     };
 
+    //REMOVEME
     struct TestGenerateToAxisUnique
     {
         auto operator()(
@@ -28,8 +96,10 @@ namespace {
         }
     };
 
+    //REMOVEME
     using TestGenerateHandlerUnique = GenerateToAxisUnique< TestGenerateToAxisUnique >;
 
+    //REMOVEME
     class GenerateToAxisUniqueTest : public ::testing::Test
     {
     public:
@@ -64,6 +134,29 @@ namespace {
 }
 
 TEST_F(
+    GenerateToAxisUnique_newTest
+    , Standard
+)
+{
+    this->test(
+        R"({
+    "min" : 10,
+    "max" : 20,
+    "deadZone" : 30,
+    "limit" : 40
+})"
+        , 10
+        , 20
+        , 30
+        , 40
+    );
+}
+
+//TODO NotExistsLimit
+//TODO FailedNotUnsignedLimit
+
+//REMOVEME
+TEST_F(
     GenerateToAxisUniqueTest
     , Standard
 )
@@ -78,6 +171,7 @@ TEST_F(
     );
 }
 
+//REMOVEME
 TEST_F(
     GenerateToAxisUniqueTest
     , NotExistsMax
@@ -92,6 +186,7 @@ TEST_F(
     );
 }
 
+//REMOVEME
 TEST_F(
     GenerateToAxisUniqueTest
     , FailedNotIntegerMax
