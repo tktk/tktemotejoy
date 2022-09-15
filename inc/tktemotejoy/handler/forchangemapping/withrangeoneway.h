94c46480a361eb42db8a99cd3fbdcd85f019df5f
tekuto <g.tekuto@gmail.com>
2022-09-15 10:18:41 +0900

    WithRangeOneWayForChangeMapping#operator()()のテストを追加

---
+++ inc/tktemotejoy/handler/forchangemapping/withrangeoneway.h
@@ -0,0 +1,54 @@
+#ifndef TKTEMOTEJOY_HANDLER_FORCHANGEMAPPING_WITHRANGEONEWAY_H
+#define TKTEMOTEJOY_HANDLER_FORCHANGEMAPPING_WITHRANGEONEWAY_H
+
+#include "tktemotejoy/handler/common/withrangeonewayimpl.h"
+#include "tktemotejoy/mappings.h"
+#include <linux/input.h>
+#include <cstddef>
+
+template< typename HANDLER_T >
+class WithRangeOneWayForChangeMapping final : public Mapping::OperateAxisHandlerForChangeMapping
+{
+    const WithRangeOneWayImpl< HANDLER_T >  IMPL;
+
+public:
+    WithRangeOneWayForChangeMapping(
+        const __s16     _MIN
+        , const __s16   _MAX
+        , const __s16   _DEAD_ZONE
+        , HANDLER_T &&  _handler
+    )
+        : IMPL(
+            _MIN
+            , _MAX
+            , _DEAD_ZONE
+            , std::move( _handler )
+        )
+    {
+    }
+
+    std::size_t operator()(
+        const __s16         _VALUE
+        , std::size_t &     _mappingIndex
+        , const std::size_t _CURRENT_MAPPING_INDEX
+    ) const override
+    {
+        //TODO
+        return 0;
+/*
+        return this->IMPL(
+            [
+                &_CURRENT_MAPPING_INDEX
+            ]
+            {
+                return _CURRENT_MAPPING_INDEX;
+            }
+            , _VALUE
+            , _mappingIndex
+            , _CURRENT_MAPPING_INDEX
+        );
+*/
+    }
+};
+
+#endif  // TKTEMOTEJOY_HANDLER_FORCHANGEMAPPING_WITHRANGEONEWAY_H
---
+++ tscripts/tktemotejoy_handler_forchangemapping_withrangeonewaytest.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from taf import *
+from taf.tools import cpp
+
+module.TYPE = module.test
+
+module.BUILDER = cpp.gtest
+
+module.TARGET = 'tktemotejoy_handler_forchangemapping_withrangeonewaytest'
+
+module.SOURCE = [
+    {
+        'handler' : {
+            'forchangemapping' : [
+                'withrangeonewaytest.cpp',
+                'dummy.cpp',
+            ],
+            'forpspstate' : [
+                'dummy.cpp',
+            ],
+        },
+    },
+    'mapping.cpp',
+    'pspstate.cpp',
+]
