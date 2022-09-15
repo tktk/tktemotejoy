c47d2f7354616e7fe7c9a16d8bb3cde9391290d2
tekuto <g.tekuto@gmail.com>
2022-09-15 13:06:09 +0900

    ToFixedAxisX#operator()()の処理を修正

--- src/tktemotejoy/handler/forpspstate/tofixedaxisx.cpp
+++ src/tktemotejoy/handler/forpspstate/tofixedaxisx.cpp
@@ -1,12 +1,10 @@
-//FIXME
 #include "tktemotejoy/handler/forpspstate/tofixedaxisx.h"
-#include "tktemotejoy/handler/forpspstate/jseventaxistopspstateaxis.h"
 #include "tktemotejoy/pspstate.h"
 
 ToFixedAxisX::ToFixedAxisX(
     const __s16 _AXIS
 )
-    : AXIS( jsEventAxisToPspStateAxis( _AXIS ) )
+    : AXIS( _AXIS )
 {
 }
 
