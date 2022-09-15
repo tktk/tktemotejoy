87ec912ab07644c4f97e395e81ca31426270a5d4
tekuto <g.tekuto@gmail.com>
2022-09-15 21:13:05 +0900

    calcMinToCenter()の処理を実装

--- src/tktemotejoy/handler/common/calcmintocenter.cpp
+++ src/tktemotejoy/handler/common/calcmintocenter.cpp
@@ -1,11 +1,11 @@
 #include "tktemotejoy/handler/common/calcmintocenter.h"
 #include <linux/input.h>
+#include <cmath>
 
 __s32 calcMinToCenter(
     const __s32     _MIN
     , const __s32   _MAX
 )
 {
-    //TODO
-    return 0;
+    return std::abs( _MAX - _MIN ) / 2;
 }
