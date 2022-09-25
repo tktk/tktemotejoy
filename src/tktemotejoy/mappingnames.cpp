77bb29bb570b6b5ab4ad430a67eae2c7a5fc52bc
tekuto <g.tekuto@gmail.com>
2022-09-25 21:38:50 +0900

    calcMappingIndex()の処理を実装

--- src/tktemotejoy/mappingnames.cpp
+++ src/tktemotejoy/mappingnames.cpp
@@ -1,10 +1,31 @@
 #include "tktemotejoy/mappingnames.h"
+#include <algorithm>
+#include <iterator>
 
 MappingNames::size_type calcMappingIndex(
     const MappingNames &                _MAPPING_NAMES
     , const MappingNames::value_type &  _MAPPING_NAME
 )
 {
+    const auto  MAPPING_NAMES_BEGIN = _MAPPING_NAMES.begin();
+    const auto  MAPPING_NAMES_END = _MAPPING_NAMES.end();
+
+    const auto  IT = std::lower_bound(
+        MAPPING_NAMES_BEGIN
+        , MAPPING_NAMES_END
+        , _MAPPING_NAME
+    );
     //TODO
-    return 100;
+/*
+    if( IT == MAPPING_NAMES_END ) {
+        throw mappingNameIsNotExists( _MAPPING_NAME );
+    }
+*/
+
+    const auto  MAPPING_INDEX = std::distance(
+        MAPPING_NAMES_BEGIN
+        , IT
+    );
+
+    return MappingNames::size_type( MAPPING_INDEX );
 }
