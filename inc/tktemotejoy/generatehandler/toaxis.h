c573fea6d295f5941630992abf8528c165de8e31
tekuto <g.tekuto@gmail.com>
2023-09-10 01:02:11 +0900

    GenerateToAxisUnique_oldを削除

--- inc/tktemotejoy/generatehandler/toaxis.h
+++ inc/tktemotejoy/generatehandler/toaxis.h
@@ -55,40 +55,4 @@ public:
     }
 };
 
-//REMOVEME
-template< typename GENERATE_TO_AXIS_UNIQUE_T >
-class GenerateToAxisUnique_old : public GenerateHandlerWithRangeUnique< GenerateToAxisUnique_old< GENERATE_TO_AXIS_UNIQUE_T > >
-{
-public:
-    auto generateHandler(
-        const Json::object_t &  _OBJECT
-        , const __s32           _MIN
-        , const __s32           _MAX
-        , const __s32           _DEAD_ZONE
-    ) const
-    {
-        const auto  KEY_LIMIT = std::string( "limit" );
-
-        const auto  DEFAULT_LIMIT = Json::number_unsigned_t(
-            calcMinToCenter(
-                _MIN
-                , _MAX
-            )
-        );
-
-        const auto &    LIMIT = getJsonUnsignedFromObjectWithDefault(
-            _OBJECT
-            , DEFAULT_LIMIT
-            , KEY_LIMIT
-        );
-
-        return GENERATE_TO_AXIS_UNIQUE_T()(
-            _MIN
-            , _MAX
-            , _DEAD_ZONE
-            , LIMIT
-        );
-    }
-};
-
 #endif  // TKTEMOTEJOY_GENERATEHANDLER_TOAXIS_H
